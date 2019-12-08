107db802147c4751e56884a916ba7f4187e399ad
r720.liaowuping.vm <348984539@qq.com>
2019-12-09 00:18:47 +0800

    stdc types

--- defs.asm
+++ defs.asm
@@ -4,3 +4,9 @@
 _VIDEO_PA       equ 0xb800      ; 实模式中显存的段地址.
 _KERNEL_SEGMENT equ 0x1000      ; 内核程序段基址.
 _KERNEL_OFFSET  equ 0x0000      ; 内核程序段偏移.
+
+%define uint8  db
+%define uint16 dw
+%define uint32 dd
+%define uint64 dq
+
